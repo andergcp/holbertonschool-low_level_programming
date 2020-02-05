a5892d155576571fdf58de41d47568ce4037bac8
andergcp <andergcp@hotmail.com>
2020-02-05 17:59:45 +0000

    6_task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main ()
+int main(void)
 {
-  printf("Size of a char: 1 byte(s)")
-  printf(" Size of an int: 4 byte(s)")
-  printf("Size of a long int: 4 byte(s)")
-  printf("Size of a long long int: 8 byte(s)")
-  printf("Size of a float: 4 byte(s)")
+printf("Size of a char: $ld byte(s)\n", sizeof(char));
+printf(" Size of an int: $ld byte(s)\n", sizeof(int));
+printf("Size of a long int: $ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: $ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: $ld byte(s)\n", sizeof(long long int));
+return (0);
 }
