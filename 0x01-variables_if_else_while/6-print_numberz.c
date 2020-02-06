251195e73be061929e054bbb286a89be44411474
andergcp <andergcp@hotmail.com>
2020-02-06 20:23:03 +0000

    sixth_taskv3

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,8 +10,8 @@
 int main(void)
 {
 	int number;
-	for (number = 0 ; number < 10; number++)
-		putchar('0'+number);
+	for (number = 0 ; number < 10 ; number++)
+		putchar('0' + number);
 	putchar('\n');
 	return (0);
 }
