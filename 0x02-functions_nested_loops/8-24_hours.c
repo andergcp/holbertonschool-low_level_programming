dc70240fbc84acc9479642f4d9ccffb55a66e9a9
andergcp <andergcp@hotmail.com>
2020-02-11 19:17:14 +0000

    fixing_8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,13 +2,14 @@
 
 void jack_bauer(void)
 {
-        int a;
+	int a;
 	int b;
 	int c;
 	int d;
+
 	for (a = '0'; a <= '2'; a++)
 	{
-		for (b = '0'; b <= '3'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
 			for (c = '0'; c <= '5'; c++)
 			{
