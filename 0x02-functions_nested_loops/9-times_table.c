a9341204c0fe56bdadfe62463e6e91951c1fb400
andergcp <andergcp@hotmail.com>
2020-02-11 19:49:46 +0000

    fixing_9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,22 +1,23 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * times_table prints the 9 times table
  *
- * Return: Always 0.
+ * Return: 9 times table
  */
 void times_table(void)
 {
-        int a;
-        int b;
-        int result;
-        for (a = 0; a <= 9; a++)
-        {
-                for(b = 0; b <= 9; b++)
-                {
+	int a;
+	int b;
+	int result;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
 			result = a * b;
 			if ((result / 10) > 0)
-				_putchar((result / 10) + '0' );
+				_putchar((result / 10) + '0');
 			else if (b != 0)
 				_putchar(' ');
 			_putchar((result % 10) + '0');
