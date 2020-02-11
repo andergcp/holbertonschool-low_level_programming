f96c363a44c0e6514d77ea6196dc1d3e1e773d49
andergcp <andergcp@hotmail.com>
2020-02-11 15:50:31 +0000

    9_task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,25 @@
  */
 void times_table(void)
 {
-	int a;
-	int b;
-	int result;
-	for (a = '0'; a <= '9'; a++)
-	{
-		for(b = '0'; b <= '9'; b++)
-		{
-			result = b * a;
-			_putchar(result / (10));
-			_putchar(result % (10));
-		}	
+        int a;
+        int b;
+        int result;
+        for (a = 0; a <= 9; a++)
+        {
+                for(b = 0; b <= 9; b++)
+                {
+			result = a * b;
+			if ((result / 10) > 0)
+				_putchar((result / 10) + '0' );
+			else
+				_putchar(' ');
+			_putchar((result % 10) + '0');
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
 	}
 }
