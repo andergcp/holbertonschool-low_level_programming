5967cfd8225c884b949ea16025aec1ea7ebe0e27
andergcp <andergcp@hotmail.com>
2020-02-11 18:11:47 +0000

    fixing_tasks_0to5

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,8 @@
 int _islower(int c)
 {
 	if (c > 96 && c < 123)
-		return(1);
+		return (1);
 	else
-		return(0);
-	return(0);
+		return (0);
+	return (0);
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,12 @@
 int _isalpha(int c)
 {
 	if (c > 96 && c < 123)
-		return(1);
+		return (1);
 	else if (c > 64 && c < 91)
-		return(1);
+		return (1);
 	else if (c > 48 && c < 58)
-                return(1);
+		return (1);
 	else
-		return(0);
-	return(0);
+		return (0);
+	return (0);
 }
