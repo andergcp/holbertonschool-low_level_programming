0639f3ea6d9c2b7259dd483be52761d2ddc1c773
andergcp <andergcp@hotmail.com>
2020-02-19 02:26:54 +0000

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s : pointer to an string
+ * Return: none
+ **/
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+
+}
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: destination copy
+ * @src: pointer to a string
+ * Return: char
+ **/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int n;
+
+	n = _strlen(src);
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
