824cf849c64d064d1c943d61e6bc6a7f08c8fe81
andergcp <andergcp@hotmail.com>
2020-02-19 02:38:04 +0000

    task9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -31,9 +31,8 @@ char *_strcpy(char *dest, char *src)
 
 	n = _strlen(src);
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
-	for (i = n - 1; i < n; i++)
-		dest[i] = '\0';
+
 	return (dest);
 }
