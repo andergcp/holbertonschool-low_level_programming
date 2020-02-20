8845f0d2fedd03bcc27a1bd5b201b3f48fad383c
andergcp <andergcp@hotmail.com>
2020-02-20 19:09:17 +0000

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ **/
+
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+			a[i] = a[i] - 32;
+		i++;
+	}
+	return (a);
+}
