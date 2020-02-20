ebd96d63f804e11959fb24cff814deaf68b340ac
andergcp <andergcp@hotmail.com>
2020-02-20 18:34:01 +0000

    fixing4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,12 +4,12 @@
  * reverse_array - reverses the content of an array of integers
  * @a: pointer to an array of integers
  * @n: int
- * Return: null 
+ * Return: null
  **/
 
 void reverse_array(int *a, int n)
 {
-	int final = n-1;
+	int final = n - 1;
 	int initial = 0;
 	int temp;
 
