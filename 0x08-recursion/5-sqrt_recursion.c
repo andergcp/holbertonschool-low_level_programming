0ad4250e3c40f3239a6a7a8641379fcda0884f2a
andergcp <andergcp@hotmail.com>
2020-02-26 22:58:25 +0000

    fixing_task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,29 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * num_root - finds the square root
+ * @m: int
+ * @n: int
+ * Return: int
  **/
 
 int num_root(int m, int n)
 {
-        if(m * m == n)
-                return(m);
-        else if (m == n || n < 0)
-		return(-1);
-        else
-                return(num_root(m + 1, n));
+	if (m * m == n)
+		return (m);
+	else if (m == n || n < 0)
+		return (-1);
+	else
+		return (num_root(m + 1, n));
 }
 
 /**
- *
- *
- *
+ * _sqrt_recursion - returns square root of an int
+ * @n: int
+ * Return: int
  **/
 
 int _sqrt_recursion(int n)
 {
-        return(num_root(1, n));                                                                                        }
+	return (num_root(1, n));
+}
