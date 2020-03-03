53f77b8e3b4bcb218cff4f8afbb06a918d8ca47e
andergcp <andergcp@hotmail.com>
2020-03-03 21:02:42 +0000

    fixing_2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,12 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+ * lenx2 - calculate the size of two strings
+ * @p1: pointer to a char
+ * @p2: pointer to a char
+ * Return: int the size of p1 + p2
+ **/
 int lenx2(char *p1, char *p2)
 {
 	int i = 0;
@@ -31,20 +37,34 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0;
 	char *p;
 
+	if (s1 == NULL)
+	{
+		s1 = malloc(1);
+		*s1 = '\0';
+	}
+	if (s2 == NULL)
+	{
+		s1 = malloc(1);
+		*s2 = '\0';
+	}
 	p = malloc((lenx2(s1, s2)) * sizeof(*p));
-
-	while (s1[i])
+	if (p != NULL)
 	{
-		p[j] = s1[i];
-		i++, j++;
-	}
-	i = 0;
+		while (s1[i])
+		{
+			p[j] = s1[i];
+			i++, j++;
+		}
+		i = 0;
 
-	while (s2[i])
-	{
-		p[j] = s2[i];
-		i++, j++;
+		while (s2[i])
+		{
+			p[j] = s2[i];
+			i++, j++;
+		}
+		p[j] = '\0';
+		return (p);
 	}
-	p[j] = '\0';
-	return (p);
+	else
+		return (NULL);
 }
