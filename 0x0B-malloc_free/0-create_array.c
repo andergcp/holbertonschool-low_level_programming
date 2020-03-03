06a807008a39c64365becf9f394e91e7c47f1e48
andergcp <andergcp@hotmail.com>
2020-03-03 18:50:40 +0000

    fixing_0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,14 +17,19 @@ char *create_array(unsigned int size, char c)
 		unsigned int i;
 
 		p = malloc(sizeof(*p) * size);
-		for (i = 0; i < size; i++)
+
+		if (p != NULL)
 		{
-			p[i] = c;
+			for (i = 0; i < size; i++)
+			{
+				p[i] = c;
+			}
+			return (p);
 		}
-		return (p);
+		else
+			return (NULL);
 	}
 	else if (size == 0)
 		return (NULL);
-	else if (p == NULL)
-		return (NULL);
+	return (0);
 }
