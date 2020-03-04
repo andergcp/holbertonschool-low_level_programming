9aa01bba8731181ae7f7ea2c0dff36a9bab83fb5
andergcp <andergcp@hotmail.com>
2020-03-04 06:46:55 +0000

    fixing6

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -26,7 +26,6 @@ int count_words(char *str)
 		}
 		i++;
 	}
-	printf("count words");
 	return (j);
 }
 
@@ -72,7 +71,7 @@ int count_chars(char *str, int word)
  * Return: the position of a char
  **/
 
-char find_char(char *str, int m, int n)
+int find_char(char *str, int m, int n)
 {
 	int i = 0, j = 0, k = 0;
 
@@ -80,7 +79,7 @@ char find_char(char *str, int m, int n)
 	{
 		if (j == m && k == n)
 		{
-			return (k);
+			return (i - 1);
 		}
 		if (str[i] != ' ')
 		{
@@ -103,7 +102,6 @@ char find_char(char *str, int m, int n)
  * @str: pointer to a char
  * Return: char
  **/
-
 char **strtow(char *str)
 {
 	int height;
@@ -112,23 +110,19 @@ char **strtow(char *str)
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-	/*
-	 * El height tiene un +1 para la posición final que debe ser NULL
-	 */
 	height = count_words(str) + 1;
 	if (height == 0)
+	{
 		return (NULL);
-
+	}
 	p = malloc(sizeof(char *) * height);
 	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < height; i++)
 	{
-		/*
-		 *Le agrego +1 a la i en a funcion counts_chars
-		 * para comenzar desde la palabra 1
-		 */
-		p[i] = malloc(sizeof(char) * (count_chars(str, (i + 1)) + 1));
+		return (NULL);
+	}
+	for (i = 0; i < height - 1; i++)
+	{
+		p[i] = malloc(sizeof(char) * ((count_chars(str, i + 1)) + 1));
 		if (p[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
@@ -137,11 +131,14 @@ char **strtow(char *str)
 			return (NULL);
 		}
 	}
-	for (m = 0; m < height; m++)
+	p[i] = malloc(sizeof(char) * 4);
+	for (m = 0; m < height - 1; m++)
 	{
 		size = count_chars(str, (m + 1));
 		for (n = 0; n < size; n++)
-			p[m][n] = find_char (str, (m + 1), (n + 1));
+			p[m][n] = str[find_char(str, m + 1, n + 1)];
+		p[m][n] = '\0';
 	}
+	p[m] = NULL;
 	return (p);
 }
