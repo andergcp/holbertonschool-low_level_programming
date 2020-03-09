4baf5b94d003ee1f63384d94989626e6ef67cb34
andergcp <andergcp@hotmail.com>
2020-03-09 21:46:25 +0000

    fixing4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,40 @@
 #include <stdlib.h>
 
 /**
+ * _strdup - check the code for Holberton School students.
+ * @str: pointer
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	int c = 0, s = 0;
+
+	char *d;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[s] != '\0')
+	{
+		s++;
+		c++;
+	}
+	c = 0;
+	d = malloc(sizeof(char) * (s + 1));
+	if (d == NULL)
+		return (NULL);
+	while (c < (s + 1))
+	{
+		d[c] = str[c];
+		c++;
+	}
+
+	if (str == NULL)
+		return (NULL);
+	return (d);
+}
+
+
+/**
  * new_dog - creates a new dog
  * @name: Pointer to a string: new dogs name
  * @age: float dogs age
@@ -12,7 +46,7 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int in = 0, io = 0, x;
+	int in = 0, io = 0;
 	dog_t *info_new;
 	char *name_cpy, *owner_cpy;
 
@@ -27,26 +61,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	while (owner[io])
 		io++;
 	io++;
-	info_new->name_cpy = malloc(sizeof(char) * in);
-	if (info_new->name_cpy == NULL)
+	info_new->name = malloc(sizeof(char) * in);
+	if (info_new->name == NULL)
 	{
-		free(info_new->name_cpy);
+		free(info_new->name);
 		free(info_new);
 		return (NULL);
 	}
-	info_new->owner_cpy = malloc(sizeof(char) * io);
-	if (info_new->owner_cpy == NULL)
+	info_new->owner = malloc(sizeof(char) * io);
+	if (info_new->owner == NULL)
 	{
-		free(info_new->owner_cpy);
-		free(info_new->name_cpy);
+		free(info_new->owner);
+		free(info_new->name);
 		free(info_new);
 		return (NULL);
 	}
 
-	for (x = 0; x <= in; x++)
-		info_new->name_cpy[x] = name[x];
-	for (x = 0; x <= io; x++)
-		info_new->owner_cpy[x] = owner[x];
+	name_cpy = _strdup(name);
+	owner_cpy = _strdup(owner);
+	info_new->name = name_cpy;
+	info_new->owner = owner_cpy;
 	info_new->age = age;
 	return (info_new);
 }
