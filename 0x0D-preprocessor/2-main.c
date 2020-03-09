a57eaaeccf9d3f36c097c227b2d2d6ea7bf0c081
andergcp <andergcp@hotmail.com>
2020-03-09 18:37:46 +0000

    initial_commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled from
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
