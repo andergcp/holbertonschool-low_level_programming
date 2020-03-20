511bbf2aaad758cba47009c0e626dc6d2c1d5fa6
andergcp <andergcp@hotmail.com>
2020-03-20 04:55:39 +0000

    fixbug

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,9 +8,9 @@ size_t print_list(const list_t *h)
 {
 	int i = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%i] %s\n", h->len, h->str);
