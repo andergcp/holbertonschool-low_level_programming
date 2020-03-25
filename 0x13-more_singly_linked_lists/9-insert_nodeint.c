88b46e19ad1fa40ee8e7458fde2862461bfd2432
andergcp <andergcp@hotmail.com>
2020-03-25 03:56:43 +0000

    [TASK] Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,18 +1,30 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ * insert_nodeint_at_index - inserts a new node at an idx position
+ * @head: double pointer to a listint_t list
+ * @idx: position to insert the new node
+ * @n: value to list attribute
+ * Return: pointer to new node if success, NULL if fails
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int counter = 0;
-	listint_t *new_node;
+	unsigned int len = 0, counter = 0; /**len includes NULL */
+	listint_t *new_node, *tmp = *head;
 
+	if (!head)
+		return (0);
+	while (tmp)
+	{
+		tmp = tmp->next;
+		len++;
+	}
+	if (idx > len)
+		return (NULL);
+	tmp = *head;
 	new_node = malloc(sizeof(listint_t));
-	if (!head || !new_node)
+	if (!new_node)
 		return (NULL);
 	new_node->n = n;
 	if (idx == 0)
@@ -22,10 +34,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{
-		while (*head)
+		while (tmp)
 		{
-			
+			if (counter == idx - 1)
+			{
+				new_node->next = tmp->next;
+				tmp->next = new_node;
+			}
+			counter++;
+			tmp = tmp->next;
 		}
 	}
-	return (*head);
+	return (new_node);
 }
