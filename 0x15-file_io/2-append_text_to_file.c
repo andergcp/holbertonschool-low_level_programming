6a46da29f37607bcd4c32be2a8b07cb5f30b7d37
andergcp <andergcp@hotmail.com>
2020-04-01 03:23:38 +0000

    [FIXING] Simplifiying validations

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,15 +16,11 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	op = open(filename, O_WRONLY | O_APPEND);
 
-	if (!text_content)
-	{
-		if (op == 1)
-			return (close(op), 1);
-		else if (op == -1)
-			return (-1);
-	}
 	if (op == -1)
-		return (-1);
+                return (-1);
+
+	if (!text_content)
+		return (1);
 
 	len = 0;
 	while (text_content[len])
@@ -34,5 +30,4 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (close(op), -1);
 	close(op);
 	return (1);
-
 }
