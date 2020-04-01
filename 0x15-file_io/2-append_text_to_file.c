1952753ffbe1007d5c0cfc2fcce29c7555d7f586
andergcp <andergcp@hotmail.com>
2020-04-01 03:14:52 +0000

    [FIXING] Task 2: Adding return -1 when text_content is valid but open isn't

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,9 +15,14 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	op = open(filename, O_RDWR | O_APPEND);
-	if (!text_content && op == 1)
-		return (close(op), 1);
-	if (!text_content && op == -1)
+	if (!text_content)
+	{
+		if (op == 1)
+			return (close(op), 1);
+		else if ( op == -1)
+			return (-1);
+	}
+	if (op == -1)
 		return (-1);
 
 	len = 0;
