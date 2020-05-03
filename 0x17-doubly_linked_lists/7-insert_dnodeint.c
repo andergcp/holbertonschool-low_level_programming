1dd086458f613798d13db0b23aa7ca896f55b047
andergcp <andergcp@hotmail.com>
2020-05-03 16:15:53 +0000

    [FIX] Replacing *head for *h

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new->next = NULL;
 		new->prev = NULL;
-		*head = new;
+		*h = new;
 		return (new);
 	}
 	counter = 0;
