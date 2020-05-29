1c4f0c58db3fd4e8e5dfe82e1e1b83b52c98073e
andergcp <andergcp@hotmail.com>
2020-05-29 20:15:44 +0000

    [FIX]

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(new_node->key, (void *)key) == 0)
 		{
 			free(new_node->value);
-			new_node->value = strdup(value);
+			new_node->value = (void *)strdup(value);
 			return (1);
 		}
 	}
