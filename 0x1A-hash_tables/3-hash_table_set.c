e7c418c494c1b6e2b48ca6c9bfed2498aa714857
andergcp <andergcp@hotmail.com>
2020-05-29 18:47:18 +0000

    [TASK] Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: hash table you want to add or update the key/value to
+ * @key: is the key of the new element
+ * @value: value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int size, index;
+	hash_node_t *new_node = NULL;
+
+	if (!ht || !value || !key || strcmp(key, "") == 0)
+		return (0);
+
+	size = ht->size;
+	index =	key_index((unsigned char *)key, size);
+
+	new_node = malloc(sizeof(hash_node_t*) * 1);
+	if (!new_node)
+		return (0);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	return (1);
+
+}
